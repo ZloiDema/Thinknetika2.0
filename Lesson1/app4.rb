8e09e39a4854113ea798afbce796fde66935eedc
Dema <grigoriidemeshkin@gmail.com>
2023-08-16 20:20:40 +0700

    Квадратное уравнение

--- Lesson1/app4.rb
+++ Lesson1/app4.rb
@@ -11,10 +11,10 @@ d = (b*b - 4 * a*c)
 
 #Проверка дискриминанта
 if d > 0
-   puts x1 = ((-1 * b + Math.sqrt(d)) / (2 * a))
-   puts x2 = ((-1 * b - Math.sqrt(d)) / (2 * a))
-  elsif d == 0
+  puts x1 = ((-1 * b + Math.sqrt(d)) / (2 * a))
+  puts x2 = ((-1 * b - Math.sqrt(d)) / (2 * a))
+elsif d == 0
   x = -1*b/2*a
-  else d < 0
-    puts "нет корней"
+else d < 0
+  puts "нет корней"
 end
